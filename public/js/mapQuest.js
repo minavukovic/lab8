fbe016d3917095c56f0090ef68076c34f837969d
minavukovic <mivukovi@ucsd.edu>
2020-02-26 11:30:38 -0800

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "wAscOwBuCJThc5Yn5kPGcAOyA7Qu6G0A";
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.8789, -117.2359]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response) {
+	var rsvpEmail = request.body.rsvpEmail; 
+
+	console.log(rsvpEmail); // print at post request
+
+	data.rsvp.push(rsvpEmail); // add to current data
+
+	// send back data
+	response.send(rsvpEmail);
+};
